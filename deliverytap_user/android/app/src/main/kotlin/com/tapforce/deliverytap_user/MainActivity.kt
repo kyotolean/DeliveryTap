ca2b5a90ab85bfd0cbbfe8fc7cdb14487f10d345
Volodymyr <vvolodya1110@gmail.com>
2022-03-11 19:02:39 +0200

    Add Login and Sign up Screen

--- deliverytap_user/android/app/src/main/kotlin/com/tapforce/deliverytap_user/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.tapforce.deliverytap_user
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- deliverytap_user/lib/utils/Colors.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flutter/material.dart';
-
-const splashBgColor = Color(0xff674fff);
-const colorPrimary = Color(0xff674fff);
-const scaffoldColorDark = Color(0xFF111111);
-const scaffoldSecondaryDark = Color(0xFF1E1E1E);
-const appButtonColorDark = Color(0xFF282828);
-const grayColor = Color(0xFF757575);
